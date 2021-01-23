174c8585d07e1ccf710181513d1982a1dd41957f
forsilence <3312034934@qq.com>
2021-01-23 21:35:29 +0800

    new version of processing_line

--- include/_M_processing_line.h
+++ include/_M_processing_line.h
@@ -5,10 +5,14 @@
 void print_line(size_t len,int total_len)
 {
 	printf("\r");
-	printf("[%d|%d]:",len,total_len);
+	printf("[%d|%d]:[",len,total_len);
 	for(int i=0;i<len;i++){
 		printf("#");
 	}
+	for(int i=len;i<total_len;++i){
+		printf(" ");
+	}
+	printf("]");
 }
 
 # endif
