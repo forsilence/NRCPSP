a96fa2ba321edd3ab5cd2b502becac7b566f80de
forsilence <3312034934@qq.com>
2021-01-23 21:41:59 +0800

    cmd version

--- main.cc
+++ main.cc
@@ -3,16 +3,43 @@
 # include<string>
 # include<ctime>
 # include"include/_GeneticAlgorithm.h"
+# include"include/_GeneticAlgorithmTest.h"
 
-int main()
+int main(int argc, char *argv[])
 {
 	std::cout << ">>> resource-constrained project scheduling problem <<<" << std::endl;
-	int popSize = 10;
-	int maxGeneration = 20;
-	double mutateP = 0.01;
-	_HeuristicAlgorithm::GeneticAlgorithm ga(popSize,maxGeneration,mutateP);
-	ga.setLoadFile("../data/j30.sm/j301_1.sm");
-	ga.setLogWriterPath("../data/j30.sm.rs/j301_1.sm");
-	ga.run();
+	_HeuristicAlgorithm::GeneticAlgorithm GA;
+	std::string loadfile;
+	std::string logfile;
+	int maxG = 100;
+	int popsize = 20;
+	if(argc>1){
+		loadfile = argv[1];
+		if(argc>2){
+			logfile = argv[2];
+			if (logfile=="x"){
+				logfile = "";
+			}
+			if(argc>3){
+				maxG = std::atoi(argv[3]);
+				if(maxG<0){
+					std::cout << "maxG must be > 0" << std::endl;
+				}
+				if(argc>4){
+					popsize = std::atoi(argv[4]);
+					if(maxG<0){
+						std::cout << "popsize must be > 0" << std::endl;
+					}
+				}
+			}
+		}
+		GA.setLoadFile(loadfile);
+		GA.setLogWriterPath(logfile);
+		GA.setMaxGeneration(maxG);
+		GA.setPopSize(popsize);
+		GA.run();
+	}else{
+		std::cout << "params : loadfile logfile(x for empty) maxG popsize mutateP crossoverP " << std::endl;
+	}
 	return 0;
 }
