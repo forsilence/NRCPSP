110cf5703c0f3b09e490fbf2ba9b31a65cff27ce
forsilence <3312034934@qq.com>
2021-01-27 18:39:46 +0800

    processing line

---
+++ src/_M_processing_line.cc
@@ -0,0 +1,17 @@
+# include"../include/_M_processing_line.h"
+void print_line(size_t len,int total_len)
+{
+	double mod_size_tmp = total_len/100.0;
+	int mod_size = (int)mod_size_tmp + (mod_size_tmp-(int)mod_size_tmp)>1 ? 1:0;
+	int in_len = len/mod_size;
+	int in_total_len = total_len/mod_size;
+	printf("\r");
+	printf("[%d|%d]:[",len,total_len);
+	for(int i=0;i<in_len;i++){
+		printf("#");
+	}
+	for(int i=len;i<in_total_len;++i){
+		printf(" ");
+	}
+	printf("]");
+}
