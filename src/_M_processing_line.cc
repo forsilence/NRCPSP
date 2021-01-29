631032a0388cd2c6311e60e6c46a27ab0dbba52e
forsilence <3312034934@qq.com>
2021-01-29 09:18:01 +0800

    processing line

--- src/_M_processing_line.cc
+++ src/_M_processing_line.cc
@@ -1,8 +1,8 @@
-# include"../include/_M_processing_line.h"
+# include"../include/M_processing_line.h"
 void print_line(size_t len,int total_len)
 {
 	double mod_size_tmp = total_len/100.0;
-	int mod_size = (int)mod_size_tmp + (mod_size_tmp-(int)mod_size_tmp)>1 ? 1:0;
+	int mod_size = (int)mod_size_tmp + ((mod_size_tmp-(double)(int)mod_size_tmp) >0.0 ? 1:0);
 	int in_len = len/mod_size;
 	int in_total_len = total_len/mod_size;
 	printf("\r");
@@ -10,8 +10,9 @@ void print_line(size_t len,int total_len)
 	for(int i=0;i<in_len;i++){
 		printf("#");
 	}
-	for(int i=len;i<in_total_len;++i){
+	for(int i=in_len;i<in_total_len;++i){
 		printf(" ");
 	}
 	printf("]");
+	printf("%f",(double)len/(double)total_len);
 }
