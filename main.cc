4e33c3a3faf2ccd3eb318e743a5dd6e268e3db90
forsilence <3312034934@qq.com>
2021-02-02 17:08:57 +0800

    easy use

--- main.cc
+++ main.cc
@@ -5,66 +5,73 @@
 # include"include/_GeneticAlgorithm.h"
 # include"include/_GeneticAlgorithmTest.h"
 # include"include/extensionOfGA.h"
+# include"include/_TemplateTest.h"
 
 int main(int argc, char *argv[])
 {
 	std::cout << ">>> resource-constrained project scheduling problem <<<" << std::endl;
-	_HeuristicAlgorithm::GeneticAlgorithm GA;
-	std::string loadfile;
-	std::string logfile;
-	int maxG = 200;
-	int popsize = 20;
-	// GA.setLogWriterPath("./data/j30.sm.rs/j303_3.csv");
-	// GA.setLoadFile("./data/j30.sm/j303_3.sm");
-	// GA.setMaxGeneration(maxG);
-	// GA.setPopSize(popsize);
-	// GA.run();
-	if(argc>1){
-		loadfile = argv[1];
-		if(argc>2){
-			logfile = argv[2];
-			if (logfile=="x"){
-				logfile = "";
-			}
-			if(argc>3){
-				maxG = std::atoi(argv[3]);
-				if(maxG<0){
-					std::cout << "maxG must be > 0" << std::endl;
-					goto label;
+	bool build_exe_file = false;
+	if(!build_exe_file){
+		_HeuristicAlgorithm::Test::TemplateTest tt;
+		tt.run_test();
+	}else{
+		_HeuristicAlgorithm::stableGA GA;
+		std::string loadfile;
+		std::string logfile;
+		int maxG = 200;
+		int popsize = 20;
+		// GA.setLogWriterPath("./data/j30.sm.rs/j303_3.csv");
+		// GA.setLoadFile("./data/j30.sm/j303_3.sm");
+		// GA.setMaxGeneration(maxG);
+		// GA.setPopSize(popsize);
+		// GA.run();
+		if(argc>1){
+			loadfile = argv[1];
+			if(argc>2){
+				logfile = argv[2];
+				if (logfile=="x"){
+					logfile = "";
 				}
-				if(argc>4){
-					popsize = std::atoi(argv[4]);
+				if(argc>3){
+					maxG = std::atoi(argv[3]);
 					if(maxG<0){
-						std::cout << "popsize must be > 0" << std::endl;
+						std::cout << "maxG must be > 0" << std::endl;
 						goto label;
 					}
-					if(argc>5){
-						double mutateP = std::atof(argv[5]);
-						if(mutateP<0){
-							std::cout << "mutateP must be > 0" << std::endl;
+					if(argc>4){
+						popsize = std::atoi(argv[4]);
+						if(maxG<0){
+							std::cout << "popsize must be > 0" << std::endl;
 							goto label;
 						}
-						GA.setMutateP(mutateP);
-						if(argc>6){
-							double crossoverP = std::atof(argv[6]);
-							if(crossoverP<0){
-								std::cout << "crossoverP must be > 0" << std::endl;
+						if(argc>5){
+							double mutateP = std::atof(argv[5]);
+							if(mutateP<0){
+								std::cout << "mutateP must be > 0" << std::endl;
 								goto label;
 							}
-							GA.setCrossoverP(crossoverP);
+							GA.setMutateP(mutateP);
+							if(argc>6){
+								double crossoverP = std::atof(argv[6]);
+								if(crossoverP<0){
+									std::cout << "crossoverP must be > 0" << std::endl;
+									goto label;
+								}
+								GA.setCrossoverP(crossoverP);
+							}
 						}
 					}
 				}
-			}
-		} 
-		GA.setLoadFile(loadfile);
-		GA.setLogWriterPath(logfile);
-		GA.setMaxGeneration(maxG);
-		GA.setPopSize(popsize);
-		GA.run();
-	}else{
-	label:
-		std::cout << "params : loadfile logfile(x for empty) maxG popsize mutateP crossoverP " << std::endl;
+			} 
+			GA.setLoadFile(loadfile);
+			GA.setLogWriterPath(logfile);
+			GA.setMaxGeneration(maxG);
+			GA.setPopSize(popsize);
+			GA.run();
+		}else{
+		label:
+			std::cout << "params : loadfile logfile(x for empty) maxG popsize mutateP crossoverP " << std::endl;
+		}
 	}
 	return 0;
 }
